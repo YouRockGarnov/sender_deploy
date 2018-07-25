aaca406f51d36776443339c83273ee904b23c8e9
Yury Garnov <hamta@yandex.ru>
2018-07-25 19:00:37 +0300

    added reset_db

--- db/creating_scratch.py
+++ db/creating_scratch.py
@@ -10,11 +10,23 @@ def create_db():
     db.connect()
     print('CONNECTED')
     # TODO сделать так, чтобы дубликаты не добавлялись
-    db.drop_tables([AdminPage, TargetGroup, UserPage, SenderPage])
-
     db.create_tables([AdminPage, TargetGroup, UserPage, SenderPage])
 
     yuri = AdminPage(vkid=142872618)
     yuri.save()
 
     db.close()
+
+def reset_db():
+    import os, psycopg2, urllib.parse as urlparse  # CHECK
+    urlparse.uses_netloc.append('postgres')
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname,
+                            port=url.port)
+
+    db.connect()
+    print('CONNECTED')
+
+    db.drop_tables([AdminPage, TargetGroup, UserPage, SenderPage])
+
+    db.create_tables([AdminPage, TargetGroup, UserPage, SenderPage])
