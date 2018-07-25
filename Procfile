e52196d25cc28ce07449326e91729665adc7354c
Yury Garnov <hamta@yandex.ru>
2018-07-25 11:44:13 +0300

    unittests don't work, i don't know why

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: FLASK_APP=main_test.py python -m flask run --host=0.0.0.0 --port=$PORT
+web: FLASK_APP=vkbot_main.py python -m flask run --host=0.0.0.0 --port=$PORT
--- main_test.py
+++ main_test.py
@@ -1,4 +1,5 @@
 from tests.request_test import AdminRequestsTest
+from db.mymodels import *
 
 from flask import Flask, json, request
 app = Flask(__name__)
@@ -8,10 +9,18 @@ def processing():
     import unittest
     print('unitests started')
     unittest.main()
+    print('unitests ended')
     return 'ok'
-
-    return 'some problems'
 
 @app.route('/', methods=['GET'])
 def index():
     return 'The app is running tests'
+
+local_debug = False
+if (local_debug):
+    import unittest
+    print('unitests started')
+    AdminPage.create(vkid=111)
+    print(AdminPage.get(AdminPage.vkid == 142872618))
+    unittest.main()
+    print('unitests ended')
