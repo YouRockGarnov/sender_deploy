9a5d7583f29b77e7a5ac80196c8bd26e5e732962
Yury Garnov <hamta@yandex.ru>
2018-07-25 19:06:58 +0300

    Added maybe working create_db()

--- db/creating_scratch.py
+++ db/creating_scratch.py
@@ -2,10 +2,27 @@ from peewee import PostgresqlDatabase
 from db.mymodels import AdminPage, TargetGroup, UserPage, SenderPage
 
 def create_db():
-    import os, psycopg2, urllib.parse as urlparse # CHECK
-    urlparse.uses_netloc.append('postgres')
-    url = urlparse.urlparse(os.environ["DATABASE_URL"])
-    db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+    import psycopg2
+    import urllib.parse as urlparse
+    import os
+    from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+    url = urlparse.urlparse(os.environ['DATABASE_URL'])
+    dbname = url.path[1:]
+    user = url.username
+    password = url.password
+    host = url.hostname
+    port = url.port
+
+    db = psycopg2.connect(
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
+
+    db.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
     db.connect()
     print('CONNECTED')
