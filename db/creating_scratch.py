d3ca9dfc92a59eb7f54550727b7dc823ba82cc91
Yury Garnov <hamta@yandex.ru>
2018-07-26 17:47:47 +0300

    Added proxy()

--- db/creating_scratch.py
+++ db/creating_scratch.py
@@ -1,22 +1,24 @@
-from peewee import PostgresqlDatabase, Database
+from peewee import PostgresqlDatabase, Database, Proxy
 from db.mymodels import AdminPage, TargetGroup, UserPage, SenderPage
 
 def create_db():
-    from app import app
-    from flask_peewee.db import PostgresqlDatabase
+    db_proxy = Proxy()
 
+    import urllib.parse as urlparse, psycopg2, os
+    urlparse.uses_netloc.append('postgres')
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+    db_proxy.initialize(db)
 
-
-    db = Database(app)
-    db.connect()
+    db_proxy.connect()
     print('CONNECTED')
     # TODO сделать так, чтобы дубликаты не добавлялись
-    db.create_tables([AdminPage, TargetGroup, UserPage, SenderPage])
+    db_proxy.create_tables([AdminPage, TargetGroup, UserPage, SenderPage])
 
     yuri = AdminPage(vkid=142872618)
     yuri.save()
 
-    db.close()
+    db_proxy.close()
 
 def reset_db():
     import os, psycopg2, urllib.parse as urlparse  # CHECK
