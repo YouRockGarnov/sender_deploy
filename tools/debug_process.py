80c74a49b1509d910cef10cf43d9477718d966d8
Yury Garnov <hamta@yandex.ru>
2018-07-28 16:48:36 +0300

    Added loads in debug_processing

--- tools/debug_process.py
+++ tools/debug_process.py
@@ -2,9 +2,11 @@ from configs.config_vkbot import confirmation_token
 from db.mymodels import db_proxy
 from tools.log import logger
 from vkbot_main import vkbot
+import json
 
+def debug_processing(strdata):
+    data = json.loads(strdata)
 
-def debug_processing(data):
     db_proxy.connect(True)
     logger.info('in processing')
 
