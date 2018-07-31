d416e53a47c5ea9d1c89c8fd7e519aa667ccf95f
Yury Garnov <hamta@yandex.ru>
2018-07-31 15:13:56 +0300

    Added return

--- bots/bot_base.py
+++ bots/bot_base.py
@@ -31,7 +31,7 @@ class BotBase:
             if user_id in self._wait_for_moderators:
 
                 logger.info('Group moderator sended url with access token.')
-                self._add_group(user_id, self._wait_for_moderators[user_id], data)
+                return self._add_group(user_id, self._wait_for_moderators[user_id], data)
 
             elif AdminPage.select().where(AdminPage.vkid == user_id).exists():
                 self.send_message(user_id, self.reply_to_admin(data))
